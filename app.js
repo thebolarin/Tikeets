e8bf03f9bed5d837e220aaf92ea08afe81260857
thebolarin <odutusinmoses@gmail.com>
2020-09-16 20:26:51 +0100

    production libraries

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const cookieSession = require('cookie-session');
 const { errorHandler, NotFoundError } = require('@bolarin/common');
 const dotenv = require("dotenv");
 require('express-async-errors');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet= require('helmet');
+const compression = require('compression');
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/event');
 const ticketRoutes = require('./routes/ticket');
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname,'access.log'),
+  {flags:'a'}
+);
+
 dotenv.config();
 const app = express();
+app.use (helmet());
+app.use(cors());
+app.use(compression());
+app.use(morgan('combined' , {stream:accessLogStream}));
+
 app.set('trust proxy', true);
 app.use(json());
 app.use(
@@ -19,6 +35,18 @@ app.use(
     secure: false,
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.use('', authRoutes);
 app.use('', eventRoutes);
 app.use('', ticketRoutes);
--- index.js
+++ index.js
@@ -22,8 +22,10 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(4000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+  const port = process.env.PORT || 3000;
+  
+  app.listen(port, () => {
+    console.log(`Listening on port  ${port}`);
   });
 };
 
